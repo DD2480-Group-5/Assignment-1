4ec53ec74b692e6b26a9680e9577d4da5be0ed9e
Ali Shahwali <ali_zinq@hotmail.com>
2023-01-28 20:20:06 +0100

    feat: Resolved issue #71
    Refactored relevant files to use JUnit v5 instead.

--- app/src/test/java/decide/CMVMethodsTest.java
+++ app/src/test/java/decide/CMVMethodsTest.java
@@ -1,7 +1,7 @@
 package decide;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.awt.geom.Point2D;
 
